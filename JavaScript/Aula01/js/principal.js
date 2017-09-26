ff807705d1106d52e7f8bbfa8b9053d663cd517e
Ruan Silva <ruanluiz.as@gmail.com>
2017-09-26 09:29:41 -0300

    atividade15,16-Aula01

---
+++ JavaScript/Aula01/js/principal.js
@@ -0,0 +1,6 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+//outra opção abaixo:
+// var titulo = document.getElementsByClassName("titulo");
+// titulo[0].textContent = "Aparecida Nutricionista";
