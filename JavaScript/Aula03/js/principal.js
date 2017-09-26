2e17c2096c7fe4828eb62c0d737e1329414d3fdc
Ruan Silva <ruanluiz.as@gmail.com>
2017-09-26 12:09:50 -0300

    atividade10,11-Aula03

--- JavaScript/Aula03/js/principal.js
+++ JavaScript/Aula03/js/principal.js
@@ -5,34 +5,44 @@ titulo.textContent = "Aparecida Nutricionista";
 // var titulo = document.getElementsByClassName("titulo");
 // titulo[0].textContent = "Aparecida Nutricionista";
 
-var trPrimeiroPaciente = document.querySelector("#primeiro-paciente");
-var tdPeso = trPrimeiroPaciente.querySelector(".info-peso");
-var tdAltura = trPrimeiroPaciente.querySelector(".info-altura");
-var tdImc = trPrimeiroPaciente.querySelector(".info-imc");
+// var trPrimeiroPaciente = document.querySelector("#primeiro-paciente"); //document.querySelector() => retorna apenas um único elemento
+var trPacientesList = document.querySelectorAll(".paciente");   //document.querySelectorAll() => retorna um array com todos elementos da classe '.paciente'
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+for(var i = 0; i < trPacientesList.length; i++){
 
-var msgLog = null;
-var alturaEhValida = true;
-var pesoEhValido = true;
+    var trPaciente = trPacientesList[i];
 
-//Operador ternário =>   test ? expression1(resultado se true) : expression2(resultado se false)
-//Validação
-if(peso <= 0 || peso >= 1000){
-    msgLog = (msgLog == null) ? "Peso inválido!" : msgLog.concat(" ","/ Peso inválido!") ;
-    pesoEhValido = false;
-}
-if(altura <= 0 || altura >= 3){
-    msgLog = (msgLog == null) ? "Altura inválida!" : msgLog.concat(" ","/ Altura inválida!") ;
-    alturaEhValida = false;
-}
-
-if(alturaEhValida && pesoEhValido){
-    var imc = peso / (altura * altura);
+    var tdPeso = trPaciente.querySelector(".info-peso");
+    var tdAltura = trPaciente.querySelector(".info-altura");
+    var tdImc = trPaciente.querySelector(".info-imc");
     
-    tdImc.textContent = imc;
-}else{
-    console.log(msgLog);
-    tdImc.textContent = msgLog;
+    var peso = tdPeso.textContent;
+    var altura = tdAltura.textContent;
+    
+    var msgLog = null;
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+    
+    //Operador ternário =>   test ? expression1(resultado se true) : expression2(resultado se false)
+    //Validação
+    if(peso <= 0 || peso >= 1000){
+        msgLog = (msgLog == null) ? "Peso inválido!" : msgLog.concat(" ","/ Peso inválido!") ;
+        pesoEhValido = false;
+    }
+    if(altura <= 0 || altura >= 3){
+        msgLog = (msgLog == null) ? "Altura inválida!" : msgLog.concat(" ","/ Altura inválida!") ;
+        alturaEhValida = false;
+    }
+    
+    if(alturaEhValida && pesoEhValido){
+        var imc = peso / (altura * altura);
+        
+        tdImc.textContent = imc.toFixed(2); // '.toFixed(2)'  => converte o imc para ser exibido com apenas 2 casas decimais
+    }else{
+        console.log(msgLog);
+        tdImc.textContent = msgLog;
+        trPaciente.classList.add("valor-invalido");
+    }
 }
+
+
