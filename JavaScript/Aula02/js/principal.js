ccdf1e7c8f7f009d6e22665b59c470f904061dd0
Ruan Silva <ruanluiz.as@gmail.com>
2017-09-26 11:11:59 -0300

    atividade11,12-Aula02

--- JavaScript/Aula02/js/principal.js
+++ JavaScript/Aula02/js/principal.js
@@ -4,3 +4,35 @@ titulo.textContent = "Aparecida Nutricionista";
 //outra opção abaixo:
 // var titulo = document.getElementsByClassName("titulo");
 // titulo[0].textContent = "Aparecida Nutricionista";
+
+var trPrimeiroPaciente = document.querySelector("#primeiro-paciente");
+var tdPeso = trPrimeiroPaciente.querySelector(".info-peso");
+var tdAltura = trPrimeiroPaciente.querySelector(".info-altura");
+var tdImc = trPrimeiroPaciente.querySelector(".info-imc");
+
+var peso = tdPeso.textContent;
+var altura = tdAltura.textContent;
+
+var msgLog = null;
+var alturaEhValida = true;
+var pesoEhValido = true;
+
+//Operador ternário =>   test ? expression1(resultado se true) : expression2(resultado se false)
+//Validação
+if(peso <= 0 || peso >= 1000){
+    msgLog = (msgLog == null) ? "Peso inválido!" : msgLog.concat(" ","/ Peso inválido!") ;
+    pesoEhValido = false;
+}
+if(altura <= 0 || altura >= 3){
+    msgLog = (msgLog == null) ? "Altura inválida!" : msgLog.concat(" ","/ Altura inválida!") ;
+    alturaEhValida = false;
+}
+
+if(alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    
+    tdImc.textContent = imc;
+}else{
+    console.log(msgLog);
+    tdImc.textContent = msgLog;
+}
