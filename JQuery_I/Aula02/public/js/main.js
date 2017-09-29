e4a8e0b192f53f0d2db744a2f9bdd139408902ef
Ruan Silva <ruanluiz.as@gmail.com>
2017-09-29 11:53:38 -0300

    atividade6-Aula02

--- JQuery_I/Aula02/public/js/main.js
+++ JQuery_I/Aula02/public/js/main.js
@@ -5,3 +5,20 @@ var numeroPalavras = frase.split(" ").length; /* a função '.split(separador)' 
                                                 espaço, e cada palavra ocupa uma posição do vetor */
 var tamanhoFrase = $("#tamanho-frase"); //acessa o span com o id tamanho-frase
 tamanhoFrase.text(numeroPalavras);
+
+var campoDigitacao = $(".campo-digitacao");
+campoDigitacao.on("input", function(){
+    var conteudoDigitado = campoDigitacao.val(); //recupera o valor do textArea
+    var qtdPalavras = (conteudoDigitado.split(/\S+/).length -1); /* o problema de utilizar o '.split(" ")' com espaço vazio, é que a validação
+                                                                    do campo é falha, pois a cada espaço vazio é contado como uma palavra. Assim,
+                                                                    se tiver uma sequencia de espaços, cada espaço é contado como palavra. Para 
+                                                                    ter uma validação mais efetiva, utilizamos a expressão regular '/\S+/' que 
+                                                                    faz a busca de qualquer caractere, exceto espaço vazio 
+
+                                                                    ##### utilizamos o '.length - 1' pois ele apresenta um bug que considera
+                                                                    um número a mais de palavras do que a quantidade real
+                                                                    */
+    var qtdCaracteres = conteudoDigitado.length;
+    $("#contador-palavras").text(qtdPalavras);
+    $("#contador-caracteres").text(qtdCaracteres);
+});
